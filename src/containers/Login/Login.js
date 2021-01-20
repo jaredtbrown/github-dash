4a1ebd61d4f1a9cb89a5b8116a8b9b55f19043f4
Jared Brown <brown.jared25@gmail.com>
2021-01-20 16:38:45 -0500

    Error handling

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,23 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import firebase from 'firebase';
 import 'firebase/auth';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Typography from '@material-ui/core/Typography';
 
 const Login = (props) => {
-    const gitHubProvider = new firebase.auth.GithubAuthProvider();
+    const [error, setError] = useState('');
 
     const signIn = async () => {
         try {
+            setError('');
+            const gitHubProvider = new firebase.auth.GithubAuthProvider();
             const result = await firebase.auth().signInWithPopup(gitHubProvider);
             const credential = result.credential;
             localStorage.setItem('token', credential.token);
             props.history.push('/');
         } catch (error) {
-            console.log(error);
+            console.error(error);
+            setError('Something went wrong. Please try again');
         }
     }
 
-    return (<Button variant="contained" color="primary" onClick={signIn}>Login with Github</Button>)
+    return (
+        <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            style={{
+                height: '100vh',
+            }}
+        >
+            <Grid item>
+                <Card>
+                    <CardHeader
+                        title={
+                            <Typography align="center" variant="h4">
+                                GitHub Dash
+                            </Typography>
+                        }
+                    />
+                    <CardContent>
+                        <Typography variant="subtitle1">GitHub Dash. A dashboard for GitHub. Login with your GitHub account to get started.</Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Grid container alignItems="center" justify="center">
+                            <Grid item>
+                                <Button variant="contained" color="primary" onClick={signIn}>
+                                    <GitHubIcon />&nbsp;Login with Github
+                                </Button>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Typography color="error">
+                                    {error}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </CardActions>
+                </Card>
+            </Grid>
+        </Grid>
+    );
 }
 
 export default Login;
--- src/theme.js
+++ src/theme.js
@@ -7,6 +7,7 @@ const theme = {
             main: '#dcffe4',
         },
     },
+    type: 'dark'
 };
 
 export default theme;
