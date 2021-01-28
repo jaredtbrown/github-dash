9e8003750e21cc19a11637e856cd9ff3ba40b683
Jared Brown <brown.jared25@gmail.com>
2021-01-27 22:49:15 -0500

    Removing unused imports

--- src/containers/Organization/Organization.js
+++ src/containers/Organization/Organization.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Icon from '@material-ui/core/Icon';
 import { GraphIcon } from '@primer/octicons-react';
